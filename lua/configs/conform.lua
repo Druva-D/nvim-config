312873c0addc94543385a8fcb405c2b07797c6f8
Druva D <ddruva445@gmail.com>
2026-09-21 16:17:26 -0400

    update format

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -2,16 +2,16 @@ local options = {
     formatters_by_ft = {
         lua = { "stylua" },
         c = { "clang-format" },
-        python = { "black" },
-        rust = {"rustfmt"}
+        -- python = { "black" },
+        rust = { "rustfmt" },
         -- css = { "prettier" },
         -- html = { "prettier" },
     },
 
     format_on_save = {
-      -- These options will be passed to conform.format()
-      timeout_ms = 5000,
-      lsp_fallback = true,  
+        -- These options will be passed to conform.format()
+        timeout_ms = 5000,
+        lsp_fallback = true,
     },
 }
 
